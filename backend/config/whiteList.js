393116b906d2fadb32edcc650156e7d39cb8d55e
iamSibashisSom <sibashis2005@gmail.com>
2025-04-03 23:47:55 +0530

    ok

--- backend/config/whiteList.js
+++ backend/config/whiteList.js
@@ -1,3 +1,3 @@
-const whiteList=['https://kaunbanegac.netlify.app', 'http://127.0.0.1:4000', 'http://localhost:4000', 'http://localhost:3000'];
+const whiteList=['https://kaunbanegac.netlify.app', 'http://127.0.0.1:4000', 'http://localhost:4000', 'http://localhost:3000', 'https://kaunbanegac.netlify.app/login', 'https://kaunbanegac.netlify.app/signUp'];
 
 module.exports=whiteList;
