df72badadb0aa3aedf66b3835bf7d6debc8f7547
iamSibashisSom <sibashis2005@gmail.com>
2025-04-04 15:03:13 +0530

    ok

--- backend/tests/questionRoutes.test.js
+++ backend/tests/questionRoutes.test.js
@@ -24,7 +24,7 @@ describe("Question API Tests", () => {
             .send({ accessToken, refreshToken }); // Include Tokens
 
         expect(res.statusCode).toBe(200);
-        expect(Array.isArray(res.body)).toBe(true);
+        expect(Array.isArray(res.body.questions)).toBe(true);
     });
 
     test(" Add a new question", async () => {
@@ -35,16 +35,60 @@ describe("Question API Tests", () => {
         };
 
         const res = await request(app)
-            .post("/admin/question")
+            .post("/admin/questions")
             .send({ ...newQuestion, accessToken,
                 refreshToken });
 
         expect(res.statusCode).toBe(201);
         expect(res.body).toHaveProperty("_id");
     });
+    test(" Add a new question", async () => {
+        const newQuestion = {
+            prompt: "What is 3 + 1?",
+            options: ["1", "2", "3", "4"],
+            correct: 3
+        };
 
-    test("Unauthorized request should fail", async () => {
+        const res = await request(app)
+            .post("/admin/questions")
+            .send({ ...newQuestion, accessToken,
+                refreshToken });
+
+        expect(res.statusCode).toBe(201);
+        expect(res.body).toHaveProperty("_id");
+    });
+    test(" Delete a question", async () => {
+        const newQuestion = {
+            prompt: "What is 3 + 2?",
+            options: ["1", "2", "3", "5"],
+            correct: 3
+        };
+
+        const req = await request(app)
+            .post("/admin/questions")
+            .send({ ...newQuestion, accessToken,
+                refreshToken });
+        const res=await request(app)
+            .delete("/admin/questions/"+req.body._id)
+            .send({ accessToken, refreshToken});
+        expect(res.statusCode).toBe(200);
+    });
+
+    test("Unauthorized admin request should fail", async () => {
         const res = await request(app).get("/admin/questions");
         expect(res.statusCode).toBe(401);
     });
+
+    test("Unauthorized user request should fail", async () => {
+        const res=await await request(app).get("/user");
+        expect(res.statusCode).toBe(401);
+    });
+
+    test("Forbidden due to wrong password", async () => {
+        const res=await request(app).post("/user").send({
+            "username":"Tata",
+            "pwd":"Steele"
+        });
+        expect(res.statusCode).toBe(401);
+    })
 });
