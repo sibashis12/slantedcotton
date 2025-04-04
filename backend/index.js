3a8e09810bc33a742138e6a615d3c5be48ab4339
iamSibashisSom <sibashis2005@gmail.com>
2025-04-04 11:49:48 +0530

    ok

--- backend/index.js
+++ backend/index.js
@@ -44,3 +44,5 @@ mongoose.connection.once('open', ()=>{
         console.log("Server running on port "+`${PORT}`);
     });
 });
+
+module.exports=app;
--- backend/tests/questionRoutes.test.js
+++ backend/tests/questionRoutes.test.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-const app = require("../server"); // Import your Express app
+const app = require("../index"); 
 const mongoose = require("mongoose");
 
 describe("Question API Tests", () => {
